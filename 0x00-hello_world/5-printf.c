13d61231ce3eb6a7f42aece404745f990def5190
Romykat81 <romaana.k7@gmail.com>
2023-04-20 02:38:20 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ *File: 5-printf.c
+ *Auth: Romykat81 <romaana.k7@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is
+ *		a piece of art," followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
