c3b5693bd45f035d35f30aa5a6fb8c456dc08a45
Romykat81 <romaana.k7@gmail.com>
2023-04-20 03:12:07 -0700

    dora korpar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 /*
- *File: 6-size.c
- *Auth Romykat81 <romaana.k7@gmail.com>
+ *
+ * File:101-quote.c
+ * Auth: Romykat81 <romaana.k7@gmail.com>
  */
 
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  *main  - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19, 
  *      followed by a new line, to standard error.
  *
- *Return: Always 1.
+ *Return: 1 if success.
  */
 int main(void)
 {
