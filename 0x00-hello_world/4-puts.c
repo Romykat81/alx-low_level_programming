88989005aa4c9b4eac53f47ee612b3b0c32bc62a
Romykat81 <romaana.k7@gmail.com>
2023-04-20 02:13:01 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,16 @@
  * File:4-puts.c
  * Auth: Romykat81 <romaana.k7@gmail.com>
  */
+
 #include <stdio.h>
+
 /**
- *main - prints "Programming is like building a multilingual puzzle, followed by a new line."
+ *main - prints "Programming is like building a multilingual 
+ 		puzzle, followed by a new line."
  *
  *Return: Always 0
  *
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
