532f6ecc6967aa29da6cbb2a6517bc4fa147a529
Romykat81 <romaana.k7@gmail.com>
2023-04-21 10:10:22 -0700

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,22 +4,22 @@
 
 /**
  * main - program that prints the alphabet in lowercase followed by a new line
- * 
+ *
  * Return: always 0.
  *
  */
 int main(void)
 {
-        char a = 'a';
+       char a = 'a';
 
-        while (a <= 'z')
-        {
-                if (a != 'q' && a!= 'e')
-                {
-                        putchar(a);
-                }
-                a++;
-        }
-        putchar ('\n');
-        return (0);
+      while (a <= 'z')
+	{
+		if (a != 'q' && a!= 'e')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar ('\n');
+	return (0);
 }
