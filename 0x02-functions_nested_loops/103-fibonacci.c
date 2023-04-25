35743f89e713e8758ea6c33886e450287bd0c6e0
Romykat81 <romaana.k7@gmail.com>
2023-04-25 07:49:29 -0700

    fibonacci sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ *main-write a program that finds and prints the sum of the even valued terms
+ *
+ *return: 0
+ */
+
+int main(void)
+{
+	unsigned long count, a, b, c, sum;
+
+	a = sum = 0;
+	b = 1;
+
+	for (count = 0; count < 50; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if (c % 2 == 0 && c < 4000000)
+		{
+			sum += c;
+		}
+		printf("%lu\n", sum);
+		return(0);
+	}
+}
