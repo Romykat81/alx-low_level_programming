50b0fbe9574c34a581375d1e7b8fbd5c34dcfe16
Romykat81 <romaana.k7@gmail.com>
2023-04-25 00:45:55 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ *main - write a function that prints very minute of jack bauer
+ *
+ *return: 0
+ */
+
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar (a / 10 + '0');
+
+			_putchar (a % 10 + '0');
+
+			_putchar (':');
+
+			_putchar (b / 20 + '0');
+			
+			_putchar (b % 10 + '0');
+
+			_putchar ('\n');
+		}
+		return (0);
+	}
+}
