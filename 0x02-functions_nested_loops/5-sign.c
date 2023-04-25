452dea79d1b952b78182e1ef75444ee039e54060
Romykat81 <romaana.k7@gmail.com>
2023-04-25 00:19:42 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ *main - print the sign of a number
+ *
+ *Return: 0,1 or -1
+ */
+
+int print_sign(int n)
+{
+	int test;
+
+	if  (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test = -1;
+		_putchar('-');
+	}
+	return (test);
+}
