2ac7baeb0116e348eecadca203056dd9bf816dfd
Romykat81 <romaana.k7@gmail.com>
2023-04-25 13:02:45 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,10 @@
 void times_table(void)
 {
 	int a, b, c;
+
 	for (a = 0; a <= 9; a++)
 	{
-		for (b =0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
 			if ((c / 10) == 0)
@@ -34,7 +35,7 @@ void times_table(void)
 			else
 			{
 				_putchar ((c / 10) + '0');
-				_putchar ((c %10) + '0');
+				_putchar ((c % 10) + '0');
 			if (b < 9)
 			{
 				_putchar(',');
