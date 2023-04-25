6fa9beaa7a3898897beef2efb78ca18bc2d4c791
Romykat81 <romaana.k7@gmail.com>
2023-04-25 14:32:12 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,12 @@ void jack_bauer(void)
 	int i, j;
 
 	i = 0;
-	while (i <24)
+	while (i < 24)
 	{
 		j = 0;
 		while (j < 60)
 		{
-			_putchar((i/10) + '0');
+			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
