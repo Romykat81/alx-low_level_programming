8c1fe28721ee1e610948f0b2a278e7e2750db74b
Romykat81 <romaana.k7@gmail.com>
2023-04-28 09:10:38 -0700

    5 more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * more_numbers - prints the numbers
  *
  * _puchar only 3 times
  * Return: 0-14 x10 and new line
- *
  */
 void more_numbers(void)
 {
-	int i;
-	int h;
-
+	int i, h;
+	
 	for (h = 0; h < 10; h++)
 	{
 		for (i = 0; i <= 14; i++)
