dafbf1947cebae58084ce83800d053760c9bb71a
Romykat81 <romaana.k7@gmail.com>
2023-05-02 04:27:56 -0700

    str len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - gets character sting
+ *
+ * @s: character to get string
+ * return: returns length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
