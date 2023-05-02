8aa1d348e5788fd737755d3e93ba471961adf1d9
Romykat81 <romaana.k7@gmail.com>
2023-05-02 08:56:59 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts_half - puts half an int
+ *
+ * @str: string to split
+ */
+void puts_half(char str)_
+{
+	int i;
+	int c = 0;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		c += 1;
+	}
+	for (i = (_strlen(str) + c / 2; i < _strlen(str); i++))
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
