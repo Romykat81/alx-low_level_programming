ce19bda95e7f408b4a9069370aa3df4c551377c0
Romykat81 <romaana.k7@gmail.com>
2023-05-04 06:16:53 -0700

    leetspeek

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/** leet - encodes a string to leetspeek
+ * @s: pointer to input
+ * Return: returns a pointer to leetspeek
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char subs[] = "aAeEoOtTlL";
+	char le[] = "43071";
+
+	i = 0;
+
+	for (i = 0; s[i] != '\0'; j++)
+	{
+		for (j = 0; subs[j] != '\0'; j++)
+			if (s[i] == subs[j])
+				s[i] = le[j / 2];
+	}
+	return (s);
+}
