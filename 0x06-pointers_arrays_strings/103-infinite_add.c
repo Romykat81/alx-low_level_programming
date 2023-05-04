65ddd0af6e1117b37584ccf07e7941090731bb47
Romykat81 <romaana.k7@gmail.com>
2023-05-04 08:05:41 -0700

    infinite loop

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,28 +10,48 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, n;
+	int i = 0, j = 0, op, bg, dr1, dr2, add = 0;
 
-	i = j = n = 0;
-
-	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
-	{
-		r[0] = 1 + '0';
-		j = 1;
-	}
-	while (i < size_r && (n1[i] != '\0' || n2[i] != '\0' || r[j] != '\0'))
-	{
-		if ((n1[i + 1] - '0') + (n2[i + 1] - '0') >= 10)
-			n = 1;
-		else
-			n = 0;
-		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
-		r[j] = r[j]% 10 + '0';
+	while(*(n1 + i) != '\0')
 		i++;
+	while (*(n2 + j) != '\0')
 		j++;
-		if (n1[i] == '\0' || n2[i] == '\0')
-			r[j] = '\0';
+
+	if (i >= j)
+		bg = i;
+	else
+		bg = j;
+
+	if (size_r <= bg + 1)
+		return (0);
+	r[bg + 1] = '\0';
+	i--, j--, size_r--;
+
+	dr1 = *(n1 + i) - 48, dr2 = *(n2 + j) - 48;
+
+	while (bg >= 0)
+	{
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+			*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (i > 0)
+			i--, dr1 = *(n1 + i) - 48;
+		else
+			dr 1 = 0;
+		if (j > 0)
+			j--, dr2 = *(n2 + j) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
-	r[j] = '\0';
-	return (r);
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
