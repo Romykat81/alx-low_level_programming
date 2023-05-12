5fa7a9b8ff0519441c1717f5875d499a5a5a3426
Romykat81 <romaana.k7@gmail.com>
2023-05-12 08:28:59 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,10 +3,10 @@
 #define UNUSED(x) (void)(x)
 
 /**
- * main - main func
- * @argc: argument c
- * @argv: vector
- * return: always 0
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
  */
 int main(int argc, char *argv[])
 {
