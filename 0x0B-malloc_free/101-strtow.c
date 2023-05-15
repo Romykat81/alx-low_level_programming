919df362de81f3be88d55476d62758e2fce7f4c0
Romykat81 <romaana.k7@gmail.com>
2023-05-15 10:31:43 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,84 +3,73 @@
 #include <stdio.h>
 
 /**
- * wordcheck-checks word
- * @word: wordcount
- * @len: length
- * @str: string to go through
- * @s: array you are assigning
- * Return: char value
+ * wordcount - helper function to count the number of words in a string
+ * @s: string to evaluate
+ *
+ * Return: number of words
  */
-char **wordcheck(int word, int len, char *str, char **s)
+int wordcount(char *s)
 {
-	int i, k, j;
+	int f, c, w;
 
-	j = 0;
-	for (i = 0; i < word; i++)
+	f = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		k = 0;
-		for ( ; j < len; j++)
+		if (s[c] == ' ')
+			f = 0;
+		else if (f == 0)
 		{
-			if (str[0] != ' ' || str[j] != ' ')
-			{
-				s[i][k] = str[j];
-				k++;
-			}
-			if (j != 0 && str[j] == ' ' && str[j - 1] != ' ')
-			{
-				j++;
-				break;
-			}
+			f = 1;
+			w++;
 		}
-		s[i][k + 1] = '\0';
 	}
-	s[word + 1] = NULL;
-	return (s);
+	return (w);
 }
 /**
- * strtow - string to words
- * @str: string to check
- * Return: return char value
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-	int i, j, k, w, size, l;
-	char **s;
+	char **m, *t;
+	int i, k = 0, l = 0, w, c = 0, st, e;
 
-	if (str == NULL)
-		return (NULL);
-	l = 0;
-	w = 0;
-	while (str[l] != '\0')
-	{
-		if (str[0] != ' ')
-			w++;
-		if (str[l] != ' ' && str[l - 1] == ' ' && l != 0)
-			w++;
+	while (*(str + l))
 		l++;
-	}
-	s = (char **)malloc(sizeof(char *) * w + 1);
-	if (s == NULL)
+	w = wordcount(str);
+	if (w == 0)
 		return (NULL);
-	j = 0;
-	for (i = 0; i < w; i++)
+
+	m = (char **)malloc(sizeof(char *) * (w + 1));
+		if (m == NULL)
+			return (NULL);
+
+	for (i = 0; i <= l; i++)
 	{
-		size = 0;
-		for (; j < l; j++)
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			if (str[0] != ' ' || str[j] != ' ')
-				size++;
-			if (str[j] == ' ' && size > 0)
-				break;
+			if (c)
+			{
+				e = i;
+				t = (char *)malloc(sizeof(char) * (c + 1));
+				if (t == NULL)
+					return (NULL);
+				while (st < e)
+					*t++ = str[st++];
+				*t = '\0';
+				m[k] = t - c;
+				k++;
+				c = 0;
+			}
 		}
-		printf("%d\n", size);
-		s[i] = (char *)malloc(sizeof(char) * size + 1);
-		if (s[i] == NULL)
-		{
-			for (k = i - 1; k >= 0; k--)
-				free(s[k]);
-			free(s);
-		}
+		else if (c++ == 0)
+			st = i;
 	}
-	s = wordcheck(w, l, str, s);
-	return (s);
+	m[k] = NULL;
+	return (m);
 }
