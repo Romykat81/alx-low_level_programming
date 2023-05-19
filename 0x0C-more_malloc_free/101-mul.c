419e89d53f45c825b2ec5da4746f48869b32ad3e
Romykat81 <romaana.k7@gmail.com>
2023-05-19 09:46:22 -0700

    fixing 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 
 #define ERR_MSG "Error"
 
@@ -59,43 +59,43 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len, len1, len2, i, carry, dig1, dig2, *res, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len 1 = _strlen(s1);
+	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
-	if (!res)
+	if (!result)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
-		res[i] = 0;
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		dig1 = s1[len1] = '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			dig2 = s2[len2] - '0';
-			carry += res[len1 + len2 + 1] + (dig1 * dig2);
-			res[len1 + len2 + 1] = carry % 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			res[len1 + len2 + 1] += carry;
+			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
-		if (res[i])
+		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(res[i] + '0');
+			_putchar(result[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
-	free(res);
+	free(result);
 	return (0);
 }
