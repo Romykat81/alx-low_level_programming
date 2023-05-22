3a9bdd2ab8679df144e0e04d6a56200f432e4854
Romykat81 <romaana.k7@gmail.com>
2023-05-22 03:26:16 -0700

    adding files to new directory

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef _PI_
+#define _PI_
+
+#define PI 3.14159265359
+typedef int make_iso_compilers_happy;
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_LIKE_MACRO_
+#define _SUM_LIKE_MACRO_
+
+#define SUM(x, y) ((x) + (y))
+typedef int make_iso_compilers_happy;
+#endif
