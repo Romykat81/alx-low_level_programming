4fc06d06d976e1f2770260190486c30d590aa238
Romykat81 <romaana.k7@gmail.com>
2023-06-06 16:05:27 -0700

    ins node fixing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,46 @@
 #include "lists.h"
 
-/*
-* insert_nodeint_at_index - ins new node
-* @head: head
-* @idx: index
-* @n: int
-* return: addr of new node
-*/
+/**
+ * insert_nodeint_at_index - inserts a node
+ * @head: head
+ * @idx: index of the list
+ * @n: int
+ *
+ * Return: the addr of node
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int i;
-    listint_t *new;
-    listint_t *h;
+	unsigned int i;
+	listint_t *new;
+	listint_t *h;
 
-    h = *head;
+	h = *head;
 
-    if (idx != 0)
-    {
-        for (i = 0; i < idx - 1 &&  h != NULL; i++)
-        {
-            h = h->next;
-        }
-    }
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 &&  h != NULL; i++)
+		{
+			h = h->next;
+		}
+	}
 
-    if (h == NULL && idx != 0)
-        return (NULL);
-    
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	if (h == NULL && idx != 0)
+		return (NULL);
 
-        new->n = n;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-        if (idx == 0)
-        {
-            new->next = *head;
-            *head = new;
-        }
-        else
-        {
-            new->next = h->next;
-            h->next = new;
-        }
-        return (new);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = h->next;
+		h->next = new;
+	}
+	return (new);
 }
