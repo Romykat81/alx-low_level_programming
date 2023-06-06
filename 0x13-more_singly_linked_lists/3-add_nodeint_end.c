e08f880817babd1b16a95b617ca8918a95c24079
Romykat81 <romaana.k7@gmail.com>
2023-06-06 15:32:18 -0700

    add node int fixing

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,37 +1,39 @@
 #include "lists.h"
 
-/*
-* add_nodeint_end - add node at end
-* @head: head of ll
-* @n: new element
-* return: addr of new elem
-*/
+/**
+ * add_nodeint_end - add a new node at the end
+ *
+ * @head: head
+ * @n: n el
+ *
+ * Return: addr of the new element.
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new;
-    listint_t *t;
+	listint_t *new;
+	listint_t *t;
 
-    (void)t;
+	(void)t;
 
-    new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
 
-    if (new == NULL)
-        return (NULL);
+	if (new == NULL)
+		return (NULL);
 
-        new->n = n;
-        new->next = NULL;
-        t = *head;
-        if (*head == NULL)
-        {
-            *head = new;
-        }
-        else
-        {
-            while (t->next != NULL)
-            {
-                t = t->next;
-            }
-            t->next = new;
-        }
-        return (*head);
+	new->n = n;
+	new->next = NULL;
+	t = *head;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (t->next != NULL)
+		{
+			t = t->next;
+		}
+		t->next = new;
+	}
+	return (*head);
 }
