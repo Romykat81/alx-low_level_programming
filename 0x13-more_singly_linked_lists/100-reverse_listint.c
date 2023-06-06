abaed85bdcb1a63defbe2e014a0e0173a37ed5de
Romykat81 <romaana.k7@gmail.com>
2023-06-06 16:38:26 -0700

    fixing 100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,25 +1,25 @@
 #include "lists.h"
 
-/*
-* reverse_listint - reverse ll
-* @head: head
-* return: ptr to node
-*/
+/**
+ * reverse_listint - reverse order
+ * @head: head
+ * Return: head
+ */
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *i;
-    listint_t *j;
+	listint_t *i;
+	listint_t *j;
 
-    i = NULL;
-    j = NULL;
+	i = NULL;
+	j = NULL;
 
-    while (*head 1= NULL)
-    {
-        j = (*head)->next;
-        (*head)->next = i;
-        i = *head;
-        *head = j;
-    }
-    *head = i;
-    return (*head);
+	while (*head != NULL)
+	{
+		j = (*head)->next;
+		(*head)->next = i;
+		i = *head;
+		*head = j;
+	}
+	*head = i;
+	return (*head);
 }
