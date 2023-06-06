9c3e0e61a5b5a578bea39f050611a515b5fc747a
Romykat81 <romaana.k7@gmail.com>
2023-06-06 15:58:59 -0700

    sum listint

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum
+ * @head: head of a list.
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
