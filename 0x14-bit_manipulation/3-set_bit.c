b306c3b495713ad72dd4b7bda16f01345ac97057
Romykat81 <romaana.k7@gmail.com>
2023-06-08 03:56:56 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-* set_bit - sets a specific bit at spec pos
-* @num: ptr to bit
-* @index: index 
-* return: 1 or -1
-*/
+ * set_bit - sets a specific bit at a pos
+ * @num: ptr
+ * @index: the pos
+ * Return: 1 or -1
+ */
 int set_bit(unsigned long int *num, unsigned int index)
 {
 	unsigned long int i = 1;
-	
-	if (index > sizeof(unsigned long int) * 8 - 1))
+
+	if (index > sizeof(unsigned long int) * 8 - 1)
 		return (-1);
-	
+
 	i <<= index;
 	*num = *num | i;
 	return (1);
