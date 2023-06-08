ed629d9a25589d94934772f49fb679b96f213796
Romykat81 <romaana.k7@gmail.com>
2023-06-08 03:31:18 -0700

    print bin

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - converts dec to bin
+ * @n: dec
+ */
+void print_binary(unsigned long int n)
+{
+	int tmp;
+	static int c;
+
+	if (n == 0 && c > 0)
+		return;
+	else if (n == 0)
+	{
+		_putchar('0);
+		return;
+	}
+
+	tmp = (n & 1);
+	c++;
+	print_binary(n >>= 1);
+	_putchar('0' + tmp);
+}
