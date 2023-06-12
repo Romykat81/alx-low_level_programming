b75c18d1eb254548e3501bd4600f05dbb565c7d5
KamiKaZe <128917461+Romykat81@users.noreply.github.com>
2023-06-12 16:56:20 +0200

    Create 0-read_textfile.c
    0-read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* read_textfile - reads a textfile and prints
+* @filename: file name
+* @letters: no. of letters printed
+* return: no. of letters printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i;
+	ssize_t j, k;
+	char *buff;
+	
+	if (!filename)
+		return (0);
+	
+	i = open(filename, O_RDONLY);
+	
+	if (i == -1)
+		return (0);
+	
+	buff = malloc(sizeof(char) * (letters));
+	if (!buff)
+		return (0);
+	
+	j = read(i, buff, letters);
+	k = write(STDOUT_FILENO, buff, j);
+	
+	close(i);
+	
+	free(buff);
+	
+	return (k);
+}
