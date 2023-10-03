4ca153d234a37640632a8e033ad27e1fb77d033d
KamiKaZe <128917461+Romykat81@users.noreply.github.com>
2023-10-03 16:47:43 +0200

    2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a new file
+ * @filename: filename
+ * @text_context: content written in file
+ *
+ * return: 1 if it is a success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int dr, letters, rdrw;
+
+	if (!filename)
+		return (-1);
+
+	dr = open(filename, O_CREAT | O_WRONLY | 0_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	 if (!text_content)
+		 text_content = " ";
+
+	 for (letters = 0; text_content[letters]; letters++)
+		 ;
+
+	 rdrw = write(dr, text_content, letters);
+
+	 if (rwr == -1)
+		 return (-1);
+
+	 close(dr);
+
+	 return (1);
+}
