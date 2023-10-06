059e96458199bf6bd9d29323573a7ad777124090
root <romaana.k7@gmail.com>
2023-10-06 06:18:06 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, letters, rdrw;
+	int fd, letters;
+	int rdrw;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | 0_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -25,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	 for (letters = 0; text_content[letters]; letters++)
 		 ;
 
-	 rdrw = write(dr, text_content, letters);
+	 rdrw = write(fd, text_content, letters);
 
 	 if (rdrw == -1)
 		 return (-1);
